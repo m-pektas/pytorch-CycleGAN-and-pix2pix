31d685b62e51469ea851167b27fab30f7e222845
muhammed.pektas <muhammed.pektas@syntonym.com>
2021-11-08 13:06:44 +0300

    Feature/Visualization : fixed data loading error.

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -18,7 +18,7 @@ class AlignedDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        self.dir_AB = os.path.join(opt.dataroot, opt.phase)  # get the image directory
+        self.dir_AB = os.path.join(opt.dataroot, opt.phase, opt.direction.split("to")[0])  # get the image directory
         self.AB_paths = sorted(make_dataset(self.dir_AB, opt.max_dataset_size))  # get image paths
         assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
         self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
